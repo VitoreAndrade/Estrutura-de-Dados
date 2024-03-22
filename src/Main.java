289505108640124be49e20eeee90f807b02c6b3a
Vitor Andrade <vitor.andradee158@gmail.com>
2024-03-22 14:04:39 -0300

    agora sim finalizado

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,9 @@ import TabelaHash.TabelaHash;
 public class Main {
     public static void main(String[] args) {
         TabelaHash tabelaClientes = new TabelaHash(18);
-        Cliente cliente1 = new Cliente("João", "08080532170", "joaoao@gmail.com", "(99)99999999");
+        Cliente cliente1 = new Cliente("João", "11111111111", "joaoao@gmail.com", "(99)99999999");
         tabelaClientes.add(cliente1);
-        Cliente cliente2 = new Cliente("João", "1", "joaoao@gmail.com", "(99)99999999");
+        Cliente cliente2 = new Cliente("João", "00000000000", "miguelel@gmail.com", "(99)99999999");
         tabelaClientes.add(cliente2);
 
         System.out.println(tabelaClientes);
